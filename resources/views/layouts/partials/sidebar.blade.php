c4c63075631aa0f53da4dc02c74f41703ab52ec0
Md. Asaduzzaman <asaduzzaman.arob@gmail.com>
2025-06-03 13:27:22 +0600

    feat: demo admin

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,8 +1,11 @@
-<aside class="sidenav navbar navbar-vertical navbar-expand-xs border-radius-lg fixed-start ms-2 bg-white my-2" id="sidenav-main">
+<aside class="sidenav navbar navbar-vertical navbar-expand-xs border-radius-lg fixed-start ms-2 bg-white my-2"
+    id="sidenav-main">
     <div class="sidenav-header">
-        <i class="fas fa-times p-3 cursor-pointer text-dark opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+        <i class="fas fa-times p-3 cursor-pointer text-dark opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
+            aria-hidden="true" id="iconSidenav"></i>
         <a class="navbar-brand px-4 py-3 m-0" href="{{ route('dashboard') }}">
-            <img src="{{ asset('assets/img/logo-ct-dark.png') }}" class="navbar-brand-img" width="26" height="26" alt="main_logo">
+            <img src="{{ asset('assets/img/logo-ct-dark.png') }}" class="navbar-brand-img" width="26" height="26"
+                alt="main_logo">
             <span class="ms-1 text-sm text-dark">{{ config('app.name') }}</span>
         </a>
     </div>
@@ -12,17 +15,34 @@
     <div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link {{ request()->routeIs('dashboard') ? 'active bg-gradient-dark text-white' : 'text-dark' }}" href="{{ route('dashboard') }}">
+                <a class="nav-link {{ request()->routeIs('dashboard') ? 'active bg-gradient-dark text-white' : 'text-dark' }}"
+                    href="{{ route('dashboard') }}">
                     <i class="material-symbols-rounded opacity-5">dashboard</i>
                     <span class="nav-link-text ms-1">Dashboard</span>
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link {{ request()->routeIs('test') ? 'active bg-gradient-dark text-white' : 'text-dark' }}" href="{{ route('test') }}">
+                <a class="nav-link {{ request()->routeIs('test') ? 'active bg-gradient-dark text-white' : 'text-dark' }}"
+                    href="{{ route('test') }}">
                     <i class="material-symbols-rounded opacity-5">test</i>
                     <span class="nav-link-text ms-1"></span>
                 </a>
             </li>
         </ul>
+    </div>
+    <div class="sidenav-footer position-absolute w-100 bottom-0">
+        <div class="mx-3">
+            <a href="javascript:;">
+                <div class="text-center">
+                    <img src="{{ asset('assets/img/team-2.jpg') }}" alt="{{ Auth::user()->name }}"
+                        class="rounded-circle" width="50" height="50">
+                    <br>
+                    <small class="text-dark mb-0">{{ Auth::user()->name }}</small>
+                    <br>
+                    <small class="text-muted text-sm">{{ Auth::user()->email }}</small>
+                </div>
+            </a>
+            <a class="btn btn-outline-dark mt-4 w-100" href="{{ route('logout') }}" type="button">Logout</a>
+        </div>
     </div>
 </aside>
