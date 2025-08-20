a72a34828b0a5dfd4db32f0faef5d78a0113bd8f
Md. Asaduzzaman <asaduzzaman.arob@gmail.com>
2025-08-20 10:08:02 +0600

    Update Helpers.php

--- app/Helpers.php
+++ app/Helpers.php
@@ -4,6 +4,21 @@ namespace App;
 
 use Illuminate\Support\Facades\Storage;
 
+if (! function_exists('imageRecover')) {
+    function imageRecover($path)
+    {
+        if (preg_match('/^https?:\/\//', $path)) {
+            return $path;
+        }
+
+        if ($path == null || ! Storage::disk(env('FILESYSTEM_DISK'))->exists($path)) {
+            return asset('assets/placeholder.png');
+        }
+
+        return asset(Storage::url($path));
+    }
+}
+
 if (! function_exists('getFileUrl')) {
     function getFileUrl($path)
     {
@@ -12,7 +27,7 @@ if (! function_exists('getFileUrl')) {
         }
 
         if ($path == null || ! Storage::disk(env('FILESYSTEM_DISK'))->exists($path)) {
-            return asset('assets/placeholder.png');
+            return null;
         }
 
         return asset(Storage::url($path));
