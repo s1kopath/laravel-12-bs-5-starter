1070d93070baf6618f130aa07b08de578238a770
Md. Asaduzzaman <asaduzzaman.arob@gmail.com>
2026-01-01 14:32:45 +0600

    Create image-preview-modal.blade.php

---
+++ resources/views/layouts/partials/image-preview-modal.blade.php
@@ -0,0 +1,13 @@
+<div class="modal fade" id="imageModal" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-lg">
+        <div class="modal-content bg-transparent border-0 shadow-none position-relative">
+            <!-- Close icon (top right inside modal) -->
+            <button type="button" class="custom-close-btn position-absolute top-0 end-0 m-2" data-bs-dismiss="modal"
+                aria-label="Close">
+                <i class="material-symbols-rounded">close</i>
+            </button>
+            <!-- Large image -->
+            <img id="modalImage" src="" class="img-fluid rounded" alt="Preview">
+        </div>
+    </div>
+</div>
